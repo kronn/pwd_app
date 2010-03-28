cdd6fadce73690e1dd1fea7f549041cb8c50016d
Matthias Viehweger <kronn@kronn.de>
2010-03-28 21:23:16 +0200

    changed inline-templates to views

--- app/pwd.rb
+++ app/pwd.rb
@@ -1,19 +1,11 @@
 require 'vendor/to_pwd/lib/password_transformation'
+require 'erb'
 
 get '/' do
-  <<-HTML
-    <html>
-      <head><title>to_pwd</title></head>
-      <body>
-        <form action="/transform" method="POST">
-          <input type="text" name="plainpass" />
-          <input type="submit" value="transform" />
-        </form>
-      </body>
-    </html>
-  HTML
+  erb :index
 end
 
 post '/transform' do
-  PasswordString.new(request.POST["plainpass"]).to_s
+  @password = PasswordString.new(request.POST["plainpass"]).to_s
+  erb :result
 end
